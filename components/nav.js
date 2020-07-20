5bdf0316da4f176d273d282602807c02dfc0dbe0
Dan <dan@ariesliving.com>
2020-07-20 10:22:32 -0400

    fix nav

--- components/nav.js
+++ components/nav.js
@@ -3,13 +3,9 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-      </ul>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
     </nav>
   )
 }
